3e8d3b671a695ba3749e626868ffff792f2ef43b
Chris Galliano <christian.galliano@linemedia.com>
2020-06-05 01:08:57 +0300

    remove uri() method from ActionInterface

--- src/Actions/ActionInterface.php
+++ src/Actions/ActionInterface.php
@@ -6,13 +6,10 @@
   use Pion\Http\Response\ResponseInterface;
   use Pion\Routing\Route\RouteInterface;
   use Pion\Templating\Engine\EngineInterface;
-  use Psr\Http\Message\UriInterface;
 
   interface ActionInterface
   {
     public static function route(): RouteInterface;
 
     public function render(EngineInterface $engine): ResponseInterface;
-
-    public function uri(): UriInterface;
   }
