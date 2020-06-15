a24a3ba7edf40396a8d41926f60e661445a4f83b
Chris Galliano <christian.galliano@linemedia.com>
2020-06-16 01:03:49 +0300

    delete redundant render method & add dock how to render response

--- src/Actions/ActionInterface.php
+++ src/Actions/ActionInterface.php
@@ -3,12 +3,13 @@
 
   namespace Pion\Actions;
 
-  use Pion\Http\Response\ResponseInterface;
   use Pion\Routing\Route\RouteInterface;
 
+  /**
+   * Action have to implement __invoke() method and return response ->
+   * @see \Pion\Http\Response\ResponseInterface
+   */
   interface ActionInterface
   {
     public static function route(): RouteInterface;
-
-    public function render(): ResponseInterface;
   }
